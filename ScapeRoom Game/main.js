61f631a1ca172395a82b50e4c6215757c2bc5f30
Rafael <rafyto21@gmail.com>
2025-07-03 12:00:27 -0500

    Add files via upload

--- ScapeRoom Game/main.js
+++ ScapeRoom Game/main.js
@@ -6,65 +6,32 @@ function randomQuestion() {
     let counter = 0;
 
     // Creating lives
-    let maxVidas = 10;
+    let maxVidas = 5;
     let vidasRestantes = maxVidas;
 
     // Welcome message
-    alert("¡Bienvenido al juego ScapeRoom!.\nPara ganar el juego debes completar el algoritmo respondiendo las preguntas.\nSi respondes 5 veces correctamente, ganas.\nTienes 10 vidas si las pierdes todas, pierdes el juego.\n¡Buena suerte!");
+    alert("¡Bienvenido al juego ScapeRoom!.\nPara ganar el juego debes completar el algoritmo respondiendo las preguntas.\nSi respondes 5 veces correctamente, ganas.\nTienes 5 vidas si las pierdes todas, pierdes el juego.\n¡Buena suerte!");
 
     // initializing the game
     while (vidasRestantes > 0) {
-
-        // Easy mode
-        if (vidasRestantes >= 5 && vidasRestantes <= 10) {
-            const easyMode = questionsBase[2];
-            let seleccionAleatoria = easyMode[Math.floor(Math.random() * easyMode.length)];
-            let preguntaAleatoria = seleccionAleatoria.pregunta;
-            let respuestaAleatoria = seleccionAleatoria.respuesta;
-            const answer = prompt(preguntaAleatoria);
-            if (answer.toLowerCase() === respuestaAleatoria.toLowerCase()) {
-                counter++;
-                if (counter === 1){
+        let seleccionAleatoria = questionsBase[Math.floor(Math.random() * questionsBase.length)];
+        let preguntaAleatoria = seleccionAleatoria.pregunta;
+        let respuestaAleatoria = seleccionAleatoria.respuesta;
+        const answer = prompt(preguntaAleatoria);
+        if (answer.toLowerCase() === respuestaAleatoria.toLowerCase()) {
+            counter++;
+            if (counter === 1){
                     alert(`¡Respuesta correcta!\nHas acertado ${counter} pregunta.`);
                 } else {
                     alert(`¡Respuesta correcta!\nHas acertado ${counter} preguntas.`);
                 }
-            } else {
-                vidasRestantes--;
-                alert(`Respuesta incorrecta. Te quedan ${vidasRestantes} vidas.`);
-            }
+        } else {
+            vidasRestantes--;
+            alert(`Respuesta incorrecta. Te quedan ${vidasRestantes} vidas.`);
         }
-
-        // Complex mode
-        else if (vidasRestantes <= 4 && vidasRestantes > 2) {
-            const complexMode = questionsBase[1];
-            let seleccionAleatoria = complexMode[Math.floor(Math.random() * complexMode.length)];
-            let preguntaAleatoria = seleccionAleatoria.pregunta;
-            let respuestaAleatoria = seleccionAleatoria.respuesta;
-            const answer = prompt(preguntaAleatoria);
-            if (answer.toLowerCase() === respuestaAleatoria.toLowerCase()) {
-                counter++;
-                alert(`¡Respuesta correcta!\nHas acertado ${counter} preguntas.`);
-            } else {
-                vidasRestantes--;
-                alert(`Respuesta incorrecta. Te quedan ${vidasRestantes} vidas.`);
-            }
-        }
-
-        // Hardcore mode
-        else if (vidasRestantes < 2 && vidasRestantes > 0) {
-            const hardcoreMode = questionsBase[0];
-            let seleccionAleatoria = hardcoreMode[Math.floor(Math.random() * hardcoreMode.length)];
-            let preguntaAleatoria = seleccionAleatoria.pregunta;
-            let respuestaAleatoria = seleccionAleatoria.respuesta;
-            const answer = prompt(preguntaAleatoria);
-            if (answer.toLowerCase() === respuestaAleatoria.toLowerCase()) {
-                counter++;
-                alert(`¡Respuesta correcta!\nHas acertado ${counter} preguntas.`);
-            } else {
-                vidasRestantes--;
-                alert(`Respuesta incorrecta. Te quedan ${vidasRestantes} vidas.`);
-            }
+        if (vidasRestantes === 0) {
+            alert("Has perdido el juego ScapeRoom. ¡Inténtalo de nuevo!");
+            break;
         }
         if(counter === 5){
             alert("¡Felicidades! Has ganado el juego ScapeRoom.");
